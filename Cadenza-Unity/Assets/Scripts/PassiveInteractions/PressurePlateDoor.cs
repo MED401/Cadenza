265dbdafae561b1313e0aec8e4556bf7ddb16c54
Rasmus Schrøder <rasmusks98@gmail.com>
2021-03-22 14:50:07 +0100

    Door closing lvl 3
    The door closes when you hit the pressure plate

---
+++ Cadenza-Unity/Assets/Scripts/PassiveInteractions/PressurePlateDoor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace PassiveInteractions
+{
+    public class PressurePlateDoor : MonoBehaviour
+    {
+        /*[SerializeField]
+        private GameObject door;
+        private void Start()
+        {
+            GameEvents.Current.OnActivateDoor += OnActivate;
+        }
+
+        private void OnActivate(int id)
+        {
+            if (GetInstanceID() != id) return;
+            
+        }
+    }*/
+        [SerializeField] private GameObject door;
+        private bool isOpened;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (isOpened) return;
+            isOpened = true;
+            door.transform.position += new Vector3(0, -4, 0);
+        }
+    }
+}
