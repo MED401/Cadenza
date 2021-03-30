1dca22eb77ff5ac1fa5e3cd3b94611b6f3d8a48e
Hector <hectorthoegersen@gmail.com>
2021-03-30 14:00:38 +0200

    Updated level 4

---
+++ Cadenza-Unity/Assets/Scripts/LevelSystem/SolutionLight.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+namespace LevelSystem
+{
+    public class SolutionLight : MonoBehaviour
+    {
+        [SerializeField] private Material onMaterial;
+        [SerializeField] private Material offMaterial;
+        private new Light light;
+
+        private void Awake()
+        {
+            offMaterial = GetComponent<MeshRenderer>().material;
+            light = gameObject.AddComponent<Light>();
+            light.enabled = false;
+            light.color = offMaterial.color; 
+        }
+
+        public void TurnOn()
+        {
+            light.enabled = true;
+            GetComponent<MeshRenderer>().material = onMaterial;
+        }
+
+        public void TurnOff()
+        {
+            light.enabled = false;
+            GetComponent<MeshRenderer>().material = offMaterial;
+        }
+    }
+
+}
