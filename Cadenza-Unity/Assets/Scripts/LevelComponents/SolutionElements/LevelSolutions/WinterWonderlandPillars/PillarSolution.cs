87d198e61a44e7d7afc912dc07c245ffd727ba38
Asger Langhoff <alangh19@student.aau.dk>
2021-04-26 11:23:06 +0200

    Finalized all functionality of winter wonderland levels

---
+++ Cadenza-Unity/Assets/Scripts/LevelComponents/SolutionElements/LevelSolutions/WinterWonderlandPillars/PillarSolution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Interactions;
+using ScriptableObjects;
+using UnityEngine;
+
+namespace LevelComponents.SolutionElements.LevelSolutions.WinterWonderlandPillars
+{
+    public class PillarSolution : Interactable
+    {
+        private AudioSource _audioSource;
+        private LevelController _levelController;
+        [SerializeField] private int pillarPedestal;
+        [SerializeField] private NoteScriptableObject _correctNote;
+
+
+        protected override void Start()
+        {
+            UseInfo = "Play Target Sound";
+            _levelController = GetComponentInParent<LevelController>();
+            _audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        public override void Interact()
+        {
+            StartCoroutine(PlayCorrectSounds(_correctNote));
+        }
+
+        private IEnumerator PlayCorrectSounds(NoteScriptableObject sound)
+        {
+            _levelController.soundObjectPlatforms[pillarPedestal-1].EnableLight();
+
+                _audioSource.clip = sound.clip;
+                _audioSource.Play();
+                yield return new WaitForSeconds(2);
+            
+
+            _audioSource.Stop();
+            _levelController.soundObjectPlatforms[pillarPedestal-1].DisableLight();
+        }
+    }
+}
